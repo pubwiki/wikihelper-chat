3965356470c9b62dbc8dd07ec7cb2c24769d6d73
undefined <yuhuison@live.com>
2025-10-08 18:38:44 +0800

    remove BotId check

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { withBotId } from "botid/next/config";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,4 +7,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default withBotId(nextConfig);
+export default nextConfig;
